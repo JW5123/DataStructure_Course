2b584e7c71d24e988893a34f02b4030b4489f6fe
JW5123 <Jay2002714@gmail.com>
2024-05-02 01:59:49 +0800

    Upload LAB10 adjacency_matrix.c

---
+++ LAB10/adjacency_matrix.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define MAX_NODES 100
+
+int main() {
+    int adjMatrix[MAX_NODES][MAX_NODES] = {0};
+    int x, y, maxNode = 0;
+
+    while (scanf("%d %d", &x, &y) != EOF) {
+        if (x == -1 && y == -1)
+            break;
+        if (x > maxNode) 
+            maxNode = x;
+        if (y > maxNode) 
+            maxNode = y;
+        adjMatrix[x][y] = 1;
+        adjMatrix[y][x] = 1;
+    }
+
+    for (int i = 0; i <= maxNode; i++) {
+        for (int j = 0; j <= maxNode; j++) {
+            printf("%d ", adjMatrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
